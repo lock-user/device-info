ee847958430eb5247ee872464a62de768ba39b77
locker <dev.private11000100@proton.me>
2023-02-03 23:26:24 +0900

    [feat] 1. 의존성 주입에 누락된 deviceinfofragment 추가

--- app/src/main/java/ch/lock/mobile/android/deviceinfo/di/FragmentModule.kt
+++ app/src/main/java/ch/lock/mobile/android/deviceinfo/di/FragmentModule.kt
@@ -1,5 +1,6 @@
 package ch.lock.mobile.android.deviceinfo.di
 
+import ch.lock.mobile.android.deviceinfo.ui.main.deviceinfo.DeviceInfoFragment
 import ch.lock.mobile.android.deviceinfo.ui.main.siminfo.SimInfoFragment
 import org.koin.androidx.fragment.dsl.fragment
 import org.koin.core.module.Module
@@ -8,6 +9,9 @@ import org.koin.dsl.module
 object FragmentModule {
 
     val INSTANCE: Module = module {
+        fragment {
+            DeviceInfoFragment()
+        }
         fragment {
             SimInfoFragment()
         }
