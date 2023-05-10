63f1d032224b5ceddaeeaa33cd6bb48850202b9e
locker <dev.private11000100@proton.me>
2023-05-10 20:28:06 +0900

    style: 미사용 import 제거

--- app/src/main/java/ch/lock/mobile/android/deviceinfo/App.kt
+++ app/src/main/java/ch/lock/mobile/android/deviceinfo/App.kt
@@ -1,18 +1,14 @@
 package ch.lock.mobile.android.deviceinfo
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.app.Application
 import android.content.Context
-import android.os.Process
-import androidx.core.app.ActivityCompat
 import ch.lock.mobile.android.deviceinfo.di.AppModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.fragment.koin.fragmentFactory
 import org.koin.core.component.KoinComponent
 import org.koin.core.context.startKoin
-import kotlin.system.exitProcess
 
 class App : Application() {
 
