1dd6db8dc57285b4063ceddd5353115c93719e48
손소독 <vot768@naver.com>
2023-11-02 12:00:15 +0900

    version: 1.0.8(10) 업그레이드

--- buildSrc/src/main/kotlin/DeviceInfo.kt
+++ buildSrc/src/main/kotlin/DeviceInfo.kt
@@ -3,8 +3,8 @@ object DeviceInfo {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 32
-    const val versionCode = 9
-    const val versionName = "1.0.7"
+    const val versionCode = 10
+    const val versionName = "1.0.8"
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
 
 }
